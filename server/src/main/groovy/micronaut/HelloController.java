af5b805b22a289bd9dbc44908fd432d2f5cd5436
bpaxton <bpaxton@talentplus.com>
2020-04-06 14:12:17 +0000

    Initial commit

---
+++ server/src/main/groovy/micronaut/HelloController.java
@@ -0,0 +1,18 @@
+package micronaut;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/movies")
+public class HelloController {
+
+    @Produces(MediaType.TEXT_PLAIN)
+    @Get("/{name}")
+    HttpResponse<String> hello(String name) {
+        return HttpResponse.ok("Hello, " + name + "!");
+    }
+
+}
